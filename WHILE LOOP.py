8b84c7435d7803950bb787cd72743d4282a6b993
mercy17ch <60229557+mercy17ch@users.noreply.github.com>
2020-03-05 20:08:40 +0300

    Add files via upload

---
+++ WHILE LOOP.py
@@ -0,0 +1,11 @@
+#while loop
+count=0
+while(count<10):
+    count=count ++1
+    print("python")
+
+#for loop
+students=['john','mary','alex']
+print(students)
+for student in students:
+    print(student)
